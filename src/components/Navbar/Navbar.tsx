ad50c38999005e7c5761555ff633b82848e147c2
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-02-10 18:47:25 +0300

    Update Navbar.tsx
    Signed-off-by: Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -47,7 +47,7 @@ const Navbar = () => {
                 <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
                     <ul className="list-reset lg:flex justify-end flex-1 items-center">
                         <li className="mr-3">
-                            <a className="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</a>
+                            <a className="inline-block text-black no-underline py-2 px-4  font-bold" href="#">Active</a>
                         </li>
                         <li className="mr-3">
                             <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
