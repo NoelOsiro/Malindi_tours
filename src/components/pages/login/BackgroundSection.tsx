6170130e78b13b57e8082319024dab1c5d20ad34
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-02-22 00:15:10 +0000

    Login background image

--- src/components/pages/login/BackgroundSection.tsx
+++ src/components/pages/login/BackgroundSection.tsx
@@ -5,7 +5,7 @@ import SocialMediaIcons from './SocialMediaIcons';
 const BackgroundSection = () => {
     return (
         <div className="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center" 
-        data-testid='background-image' style={{ backgroundImage: "url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80)" }}>
+        data-testid='background-image' style={{ backgroundImage: "url(https://source.unsplash.com/random/?coralreef)" }}>
                     <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
                     <div className="w-full px-24 z-10">
                         <h1 className="text-5xl font-bold text-left tracking-wide">Keep it special</h1>
