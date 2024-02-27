7cf7a80e9507a2d6dbd892521bc07bf29a7d5c89
Azubi Forex <157981015+coastcodermal@users.noreply.github.com>
2024-02-28 00:24:44 +0300

    Update page.tsx
    Signed-off-by: Azubi Forex <157981015+coastcodermal@users.noreply.github.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { ChangeEvent, MouseEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import BackgroundSection from '../../components/pages/login/BackgroundSection';
